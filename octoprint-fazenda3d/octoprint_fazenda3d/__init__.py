c84b0de0922006b866e1b64737601e05b2951abc
GuilhermeHMFroes <guil.henrique.fr@gmail.com>
2025-09-23 19:23:08 -0300

    procurando o erro volume 9.3

--- octoprint-fazenda3d/octoprint_fazenda3d/__init__.py
+++ octoprint-fazenda3d/octoprint_fazenda3d/__init__.py
@@ -59,7 +59,7 @@ class Fazenda3DPlugin(octoprint.plugin.SettingsPlugin,
         token = self._settings.get(["token"])
         nome = self._settings.get(["nome_impressora"])
 
-        self._logger.info("Entrou no loop. token: {token}").f
+        self._logger.info("Entrou no loop. token: {}").format(token)
 
         if not servidor or not token:
             # não configurado ainda
